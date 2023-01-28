006a058d86d4a4ac2fa9a128e59f5a47e278a41b
diego <perezdi1@msu.edu>
2023-01-28 17:50:44 -0500

    Commit

--- lib/tutorial.dart
+++ lib/tutorial.dart
@@ -9,10 +9,23 @@ class TutorialPage extends StatefulWidget {
 }
 
   class _TutorialPageState extends State<TutorialPage>{
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }
+    @override
+    Widget build(BuildContext context) {
+      return Padding(
+          padding: const EdgeInsets.all(10),
+        child: ListView(
+        children: <Widget>[
+            Container(
+            height: 50,
+            padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: ElevatedButton(
+              child: const Text('Continue'),
+              onPressed: () {
+                Navigator.push(context,MaterialPageRoute(builder: (context)=>TutorialPage()));
+              },
+            )
+        )
+      ]
+      ));
+    }
   }
