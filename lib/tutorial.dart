a1a54d830583ea6565bfc8d3676234781b14aba1
Rkochhar7 <100734057+Rkochhar7@users.noreply.github.com>
2023-01-28 19:37:36 -0500

    Update tutorial.dart

--- lib/tutorial.dart
+++ lib/tutorial.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hackathon/main.dart';
+import 'package:hackathon/questionnaire.dart';
 
 class TutorialPage extends StatefulWidget {
   const TutorialPage({Key? key}) : super(key: key);
@@ -8,24 +9,24 @@ class TutorialPage extends StatefulWidget {
   State<TutorialPage> createState() => _TutorialPageState();
 }
 
-  class _TutorialPageState extends State<TutorialPage>{
-    @override
-    Widget build(BuildContext context) {
-      return Padding(
-          padding: const EdgeInsets.all(10),
+class _TutorialPageState extends State<TutorialPage>{
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(10),
         child: ListView(
-        children: <Widget>[
-            Container(
-            height: 50,
-            padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-            child: ElevatedButton(
-              child: const Text('Continue'),
-              onPressed: () {
-                Navigator.push(context,MaterialPageRoute(builder: (context)=>TutorialPage()));
-              },
-            )
-        )
-      ]
-      ));
-    }
+            children: <Widget>[
+              Container(
+                  height: 50,
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: ElevatedButton(
+                    child: const Text('Continue'),
+                    onPressed: () {
+                      Navigator.push(context,MaterialPageRoute(builder: (context)=>QuestionnairePage()));
+                    },
+                  )
+              )
+            ]
+        ));
   }
+}
