aaaf2a8bc696c9afd0fcd99b1cd0199f8643cb85
christiananovert <anovertc@msu.msu>
2023-01-28 23:58:19 -0500

    Questions file/dart

--- lib/questionare.dart
+++ lib/questionare.dart
@@ -1,3 +1,31 @@
+import 'package:flutter/material.dart';
+
+class QuestionnairePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [
+          Container(
+              height: 50,
+              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: ElevatedButton(
+                child: const Text('next'),
+                onPressed: () {
+                  Navigator.push(context,MaterialPageRoute(builder: (context)=>QuestionnairePage()));
+                },
+              )
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
 class Question {
   final String text;
   final List<Option> options;
