d75d9d3fb076759cb2a2cc6c06bac4399fe4ffa9
diego <perezdi1@msu.edu>
2023-01-28 20:40:03 -0500

    Commit

--- lib/tutorial.dart
+++ lib/tutorial.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hackathon/main.dart';
-import 'package:hackathon/questionnaire.dart';
+import 'package:hackathon/questionare.dart';
 
 class TutorialPage extends StatefulWidget {
   const TutorialPage({Key? key}) : super(key: key);
@@ -9,24 +9,41 @@ class TutorialPage extends StatefulWidget {
   State<TutorialPage> createState() => _TutorialPageState();
 }
 
-class _TutorialPageState extends State<TutorialPage>{
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(10),
+  class _TutorialPageState extends State<TutorialPage>{
+    @override
+    Widget build(BuildContext context) {
+      return Padding(
+          padding: const EdgeInsets.all(10),
         child: ListView(
-            children: <Widget>[
-              Container(
-                  height: 50,
-                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                  child: ElevatedButton(
-                    child: const Text('Continue'),
-                    onPressed: () {
-                      Navigator.push(context,MaterialPageRoute(builder: (context)=>QuestionnairePage()));
-                    },
-                  )
-              )
-            ]
-        ));
+        children: <Widget>[
+          const Card(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text('Hello World!',
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(fontWeight: FontWeight.bold),
+
+            ),
+          )
+          ),
+            Container(
+            height: 50,
+            padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: ElevatedButton(
+              child: const Text('Continue'),
+              onPressed: () {
+                Navigator.push(context,MaterialPageRoute(builder: (context)=>QuestionnairePage()));
+              },
+            )
+            )
+      ]
+      ));
+
+    }
+
   }
-}
+
+
+
+
